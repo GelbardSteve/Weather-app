61e116606ee5ca0446155391fbf0fbcb7e953171
Steve Gelbard <steve.g@webpals.com>
2019-08-07 16:12:44 +0300

    new design

--- src/app/wather/wather.component.ts
+++ src/app/wather/wather.component.ts
@@ -1,33 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { ApixuService } from "../apixu.service";
 
 @Component({
-  selector: 'app-wather',
-  templateUrl: './wather.component.html',
-  styleUrls: ['./wather.component.scss']
+  selector: "app-wather",
+  templateUrl: "./wather.component.html",
+  styleUrls: ["./wather.component.scss"]
 })
 export class WatherComponent implements OnInit {
-
   weatherSearchForm: FormGroup;
   weatherData: Object;
 
   constructor(
     private formBuilder: FormBuilder,
     private apixuService: ApixuService
-    ) { }
+  ) {}
 
   ngOnInit() {
     this.weatherSearchForm = this.formBuilder.group({
-      location: ['', Validators.required]
+      location: ["", Validators.required]
     });
   }
 
   sendToAPIXU(formValues) {
     this.apixuService
-    .getWeather(formValues.location)
-    .subscribe(data => this.weatherData = data);
-    console.log(this.weatherData)
+      .getWeather(formValues.location)
+      .subscribe(data => (this.weatherData = data));
+    console.log(this.weatherData);
   }
-
 }
